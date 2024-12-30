3237245d6cecfcbe451a439629cb67f28663bf41
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-12-30 11:59:30 +0100

    Added basic styling to board

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css:['~/assets/css/vello.css'],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxt/ui"],
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
-<template>
-  <h1>Build a Trello Clone Home Page</h1>
-  <pre>{{ board }}</pre>
-</template>
-
 <script lang="ts" setup>
 import { useBoardStore } from '~/stores/boardStore';
 
 const { board } = useBoardStore();
 
 </script>
+
+<template>
+  <div class="board-wrapper">
+    <main class="board">
+      <UContainer v-for="column in board.columns" :key="column.name" class="column">
+        <h2 class="mb-4">{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+            <UCard class="mb-4">
+              <strong>{{ task.name }}</strong>
+              <p>{{ task.description }}</p>
+            </UCard>
+          </li>
+        </ul>
+      </UContainer>
+    </main>
+  </div>
+</template>
 
 <style></style>
