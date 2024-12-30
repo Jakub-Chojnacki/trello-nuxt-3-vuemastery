2df8cc5bffa156122e7a500d990d59cc3b73c992
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-12-30 11:48:03 +0100

    Add first store, install pinia and nuxt-ui modules, force light mode

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Build a Trello Clone Home Page</h1>
+  <pre>{{ board }}</pre>
+</template>
+
+<script lang="ts" setup>
+import { useBoardStore } from '~/stores/boardStore';
+
+const { board } = useBoardStore();
+
+</script>
+
+<style></style>
